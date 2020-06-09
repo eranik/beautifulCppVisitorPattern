79caaab13efec660866baae69c477a194703c698
a <a@a.a>
2020-06-09 16:07:59 -0700

    minor update and renaming

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-mv ./VisitorWithDataInAndOut.exe /tmp
-g++ -O2 -std=c++2a ./VisitorWithDataInAndOut.cpp  -o VisitorWithDataInAndOut.exe |& tee VisitorWithDataInAndOut.log
+mv ./BeautifulModernVisitorPatternWithData.exe /tmp 2>/dev/null
+g++ -O2 -std=c++2a ./BeautifulModernVisitorPatternWithData.cpp  -o BeautifulModernVisitorPatternWithData.exe |& tee BeautifulModernVisitorPatternWithData.log
 
